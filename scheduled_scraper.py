edcbfb1889bc9ff7c910a33f6702727a988a5fa3
Derrick J Chie <djchie.dev@gmail.com>
2015-10-12 23:32:27 -0700

    Changes the time for scheduled jobs to test faster

--- scheduled_scraper.py
+++ scheduled_scraper.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('interval', minutes=1)
 def timed_job():
     print('This job is run every three minutes.')
 
