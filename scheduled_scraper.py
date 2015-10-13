c4eb53a50863bb7c5bd4ceec2290d49061262eb1
Derrick J Chie <djchie.dev@gmail.com>
2015-10-12 23:50:43 -0700

    Adds new verion of APScheduler

--- scheduled_scraper.py
+++ scheduled_scraper.py
@@ -2,12 +2,10 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    print('This job is run every three minutes.')
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+@sched.scheduled_job('interval', minutes=1)
+def job():
+    print 'This job is run every minute.'
+
 
 sched.start()
